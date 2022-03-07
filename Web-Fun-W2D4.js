f4c0250941c2e084ac02a83d3c06db1c55258613
Itserge1 <realitserge@gmail.com>
2022-03-07 10:49:55 -0500

    2 dimensional array

--- Web-Fun-W2D4.js
+++ Web-Fun-W2D4.js
@@ -1,69 +1,24 @@
-var pokémon = [
-    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
-    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
-    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
-    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
-    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
-    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
-    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
-    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
-    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
-    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
-    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
-    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
-    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
-    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
-    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
-    { "id": 98,  "name": "Krabby",     "types": ["water"] },
-    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
-    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
-    { "id": 133, "name": "Eevee",      "types": ["normal"] },
-    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
-    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
-    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
-    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
-];
+// Given a 2 dimensional array (an array containing arrays), return a new array containing just 
+// the values from inside the sub-arrays.Don't assume the array will always be the same size, or 
+// that the sub-arrays are all the same length (the array might be jagged). Don't use built-in 
+// methods like Array.prototype.flat() but feel free to use .push(value) and/or .pop() where needed.
 
-// console.log the pokémon objects whose id is evenly divisible by 3
-function pokemonIdDivisibleBy3(){
-    for(let i=0; i<pokémon.length; i++){
-        if(pokémon[i].id % 3 == 0){
-            console.log(pokémon[i])
+
+
+
+// complete the following function
+function flatten(arr2d) {
+    var flat = [];
+    // your code here
+    for(let i=0; i<arr2d.length; i++){
+        for(let j=0; j<arr2d[i].length; j++){
+            flat.push(arr2d[i][j]);
         }
     }
-}
-pokemonIdDivisibleBy3()
+    // console.log(flat.length)
 
-// console.log the pokémon objects that have more than one type
-function pokemonTypeMoreThanOne(){
-    for (let i=0; i<pokémon.length; i++){
-        if(pokémon[i].types.length >1){
-            console.log(pokémon[i])
-        }
-    }
+    return flat;
 }
-pokemonTypeMoreThanOne()
-
-// console.log the names of the pokémon whose only type is "poison"
-function pokemonTypePoison(){
-    for (let i=0; i<pokémon.length; i++){
-        if(pokémon[i].types.length == 1 && pokémon[i].types[0] ==="poison"){
-            console.log(pokémon[i]);
-        }
-    }
-}
-pokemonTypePoison()
-
-// console.log the first type of all the pokémon whose second type is "flying"
-
-function FirstPokemonSecondTypeFliying(){
-    let typeFlying = [];
-    for(let i=0; i<pokémon.length; i++){
-        if(pokémon[i].types[1] == "flying"){
-            typeFlying.push(pokémon[i]);
-        }
-    }
-    console.log(typeFlying[0])
-    // console.log(typeFlying.length);
-}
-FirstPokemonSecondTypeFliying()
+    
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
