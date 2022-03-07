459f2bbfac2d689be1b2d8580f48481f5420702f
Itserge1 <realitserge@gmail.com>
2022-03-07 09:12:03 -0500

    While loop

--- PredictTheOutputW1D4.js
+++ PredictTheOutputW1D4.js
@@ -1,60 +1,60 @@
 // ** 1
 
-// var x = 5;
+var x = 5;
     
-// function setX(newValue) {
-//     x = newValue; 
-// }
+function setX(newValue) {
+    x = newValue; 
+}
     
-// console.log(x);
-// setX(15);
-// console.log(x);
+console.log(x);
+setX(15);
+console.log(x);
 
-// // output
-// // 5
-// // 15
+// output
+// 5
+// 15
 
 // ** 2
 
-// var x = 5;
+var x = 5;
     
-// function addToX(amount) {
-//     return x + amount;
-//     console.log("hello there");
-// }
+function addToX(amount) {
+    return x + amount;
+    console.log("hello there");
+}
     
-// console.log(x);
-// var result = addToX(-10);
-// console.log(result);
-// console.log(x);
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
 
-// // Output
-// // 5
-// // -5
-// // 5
+// Output
+// 5
+// -5
+// 5
 
 // ** 3
 
-// function isPal(arr) {
-//     for(var left=0; left<arr.length/2; left++) {
-//         var right = arr.length-1-left;
-//         if(arr[left] != arr[right]) {
-//             return "Not a pal-indrome!";
-//         }
-//     }
-//     return "Pal-indrome!";
-// }
+function isPal(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+        var right = arr.length-1-left;
+        if(arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
+}
     
-// var result1 = isPal( [1, 1, 2, 2, 1] );
-// console.log(result1);
+var result1 = isPal( [1, 1, 2, 2, 1] );
+console.log(result1);
     
-// var result2 = isPal( [3, 2, 1, 1, 2, 3] );
-// console.log(result2)
+var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+console.log(result2)
 
-// var result3 = isPal( [3, 2, 1, 2, 3] );
-// console.log(result3)
+var result3 = isPal( [3, 2, 1, 2, 3] );
+console.log(result3)
 
-// // Output
-// // Not a pal-indrome!
-// // Pal-indrome!
-// // Pal-indrome!
+// Output
+// Not a pal-indrome!
+// Pal-indrome!
+// Pal-indrome!
