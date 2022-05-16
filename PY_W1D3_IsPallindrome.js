8b4bc006c1098b42b9b3832ba59a52f62dd38f76
Itserge1 <realitserge@gmail.com>
2022-05-16 08:55:10 -0400

    Is pallindrome

---
+++ PY_W1D3_IsPallindrome.js
@@ -0,0 +1,27 @@
+// Create a function that returns as boolean of true/false for whether or not an input string is a strict pallindrome. Do not ignore whitespaces, casing matters!!
+
+// Example 1: "racecar" --> true
+// Example 2: "Dud" --> false
+// Example 3: "oho!" --> false
+
+// pallidrome = reads the same forwards and back!
+// loop through our string
+// check each element with it's 'sister' element on the other side of the string
+// if elements don't match then return false
+// if we make it through our loop and never hit false, then return true
+
+const IsPallindrome = (str) =>{
+    let NewStr ="";
+    for(let i = str.length -1; i > -1; i--){
+        NewStr += str[i];
+    }
+    if(NewStr == str){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+// console.log(IsPallindrome("racecar"));
+// console.log(IsPallindrome("Dud"));
+// console.log(IsPallindrome("oho!"));
