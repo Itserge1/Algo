1f8071218f6585498dbda76ffd0c69a764d97c24
Itserge1 <realitserge@gmail.com>
2022-06-29 11:35:57 -0400

    Added other version of the function

--- PY_W1D3_IsPallindrome.js
+++ PY_W1D3_IsPallindrome.js
@@ -12,31 +12,86 @@
 
 
 
-// const IsPallindrome = (str) =>{
-//     let NewStr ="";
-//     for(let i = str.length -1; i > -1; i--){
-//         NewStr += str[i];
-//     }
-//     if(NewStr == str){
-//         return true;
-//     } else{
-//         return false;
-//     }
-// }
+const isPallindrome = (str) =>{
+    let NewStr ="";
+    for(let i = str.length -1; i > -1; i--){
+        NewStr += str[i];
+    }
+    if(NewStr == str){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+// console.log(isPallindrome("AmnbA")); // FALSE
+// console.log(isPallindrome("racecar")); // TRUE
+// console.log(isPallindrome("Dud")); // TRUE
+// console.log(isPallindrome("oho!")); // ?
+
 
 function isPallindrome(str) {
     // loop through our string
-        for ( var i = 0 ; i < str.length/2 ; i ++) {
+    for (var i = 0; i < str.length / 2; i++) {
         // check each element with it's 'sister' element on the other side of the string
         // if elements don't match then return false
-            if (str[i] != str[str.length - 1 - i]) {
-                return false;
-            }
+        // console.log(str[i].toLowerCase());
+        // console.log(str[str.length -1 -i].toLowerCase());
+        if (str[i].toLowerCase() != str[str.length - 1 - i].toLowerCase()) {
+            return false;
         }
-        // if we make it through our loop and never hit false, then return true
-        return true;
     }
+    // if we make it through our loop and never hit false, then return true
+    return true;
+}
 
-console.log(isPallindrome("racecar"));
-console.log(isPallindrome("Dud"));
-console.log(isPallindrome("oho!"));
+// console.log(isPallindrome("AmnbA")); // FALSE
+// console.log(isPallindrome("racecar")); // TRUE
+// console.log(isPallindrome("Dud")); // ?
+// console.log(isPallindrome("oho!")); // ?
+
+
+function isPallindrome(str) {
+    // your code here
+    let result = true;
+    for (var i = 0; i < str.length / 2; i++) {
+        if (result){
+            // console.log(str[i].toLowerCase());
+            // console.log(str[str.length - 1 - i].toLowerCase());
+            if (str[i].toLowerCase() == str[str.length - 1 - i].toLowerCase()) {
+                result = true;
+            } else{
+                result = false
+            }
+
+        } else{
+            return false
+        }
+    }
+    return result;
+}
+
+// console.log(isPallindrome("AmnbA")); // FALSE
+// console.log(isPallindrome("racecar")); // TRUE
+// console.log(isPallindrome("Dud")); // ?
+// console.log(isPallindrome("oho!")); // ?
+
+function isPallindrome(str) {
+    let newArr = [];
+    for (let i = str.length; i >= 0; i--) {
+        newArr.push(str[i]);
+    }
+    // newArr.join('')
+    if (str == newArr.join('')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// console.log(isPallindrome("AmnbA")); // FALSE
+// console.log(isPallindrome("racecar")); // TRUE
+// console.log(isPallindrome("Dud")); // ?
+// console.log(isPallindrome("oho!")); // ?
+
+
