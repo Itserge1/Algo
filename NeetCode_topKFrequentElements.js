cf5959f1a05e44d5bf8d0b6b7cb91e819d096101
Itserge1 <realitserge@gmail.com>
2023-06-27 13:01:12 -0400

    top frequency element

---
+++ NeetCode_topKFrequentElements.js
@@ -0,0 +1,29 @@
+// # Given an integer array nums and an integer k, 
+// # return the k most frequent elements. You may return the answer in any order.
+
+// # Input: nums = [1,1,1,2,2,3], k = 2
+// # Output: [1,2]
+
+// # Input: nums = [1], k = 1
+// # Output: [1]
+
+
+var topKFrequent = function(nums, k) {
+    let frequency = {}
+    for( let i = 0; i < nums.length; i++){
+        if(frequency.hasOwnProperty(nums[i])) frequency[nums[i]] += 1;
+        else frequency[nums[i]] = 1;
+    }
+    let result = Object.keys(frequency).map((key) => [Number(key), frequency[key]]);
+    let sortedResult = result.sort((a,b) => {
+        return b[1]-a[1]
+    })
+    let output = []
+    for ( let i = 0; i < k; i++){
+        output.push(sortedResult[i][0])
+    }
+    return output;
+};
+
+console.log(topKFrequent([1,1,1,2,2,3], 2))
+console.log(topKFrequent([1], 1))
