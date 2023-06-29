4d434aef2f457e42853a3f93d4c7b9160bab0b1f
Itserge1 <realitserge@gmail.com>
2023-06-29 16:43:13 -0400

    revise two sum

--- NeetCode_TwoSum_1.js
+++ NeetCode_TwoSum_1.js
@@ -54,4 +54,4 @@ const twoSum = (arr, target) => {
 
 console.log(twoSum([2,7,11,15], 9)); // [0,1]
 console.log(twoSum([3,2,4], 6)); // [1,2]
-console.log(twoSum([3,3], 6)); // [0,1]
+console.log(twoSum([3,3], 6)); //  [0,1]
